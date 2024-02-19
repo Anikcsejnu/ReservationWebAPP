15fe8a6505d5ef7b33cb9e7cde15925ed02606ae
Atikur Rhaman <anikraju110@gmail.com>
2024-02-19 23:49:36 +0600

    Add home and about handler and refactor code

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 3)
+	_, _ = fmt.Fprint(w, fmt.Sprintf("This is the about page and 2 + 3 is %d", sum))
+}
+
+// AddValue add two integer and return sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main in the main application function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of Byte written: %d", n))
-	})
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil)
 }
