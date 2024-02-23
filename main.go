ba31e8d33919409fa5c19ba24c7e627749ee9b62
Atikur Rhaman <anikraju110@gmail.com>
2024-02-24 00:26:12 +0600

    Reorganize the code base and add few UI changes

--- main.go
+++ main.go
@@ -3,20 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
 
 // main in the main application function
 func main() {
@@ -25,13 +14,4 @@ func main() {
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error on parsing template:", err)
-	}
 }
